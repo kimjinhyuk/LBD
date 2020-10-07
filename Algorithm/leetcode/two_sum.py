686224c4e99277c7f0731075feb02ffa93f165fd
kimjinhyuk <kimjinhyuk2@gmail.com>
2020-10-08 00:06:13 +0900

    Leetcode Array101

---
+++ Algorithm/leetcode/two_sum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return len(max("".join(map(str, nums)).split("0")))
+
+
+# sonlutionInstance = Solution()
+# findMaxConsecutiveOnes([0, 1, 1, 0, 0, 1, 1, 1, 0])
+
