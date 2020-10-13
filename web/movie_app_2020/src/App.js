6241a9366b07c32c3a446386f2d59472b4153675
kimjinhyuk <kimjinhyuk2@gmail.com>
2020-10-14 00:24:26 +0900

    practice components with JSX + Props

--- web/movie_app_2020/src/App.js
+++ web/movie_app_2020/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+function Food({ fav }) {
+  return <h3> I love {fav}</h3>;
+}
+
 function App() {
   return (
-    <div> hello! </div>
+    <div>
+      <h1>hello! </h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="삼겹살" />
+      <Food fav="쭈꾸미" />
+      {/* Food component property fav value kimchi */}
+    </div>
   );
 }
 
