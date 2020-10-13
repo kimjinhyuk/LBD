9bc705540fffcfc62229c216c7e7f695fbfc7b1a
kimjinhyuk <kimjinhyuk2@gmail.com>
2020-10-13 23:28:27 +0900

    update app.js

--- web/react-app/src/App.js
+++ web/react-app/src/App.js
@@ -2,16 +2,6 @@ import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 import './App.css';
 
-function Subjuct() extends Component {
-  render(){
-    return (
-      <header>
-        <h1>WEB</h1>
-      </header>
-    );
-  }
-}
-
 function App() {
   return (
     <div className="App">
