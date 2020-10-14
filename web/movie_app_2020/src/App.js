5231cb23932b2706ac72f09102bb392696c7e534
kimjinhyuk <kimjinhyuk2@gmail.com>
2020-10-14 22:20:32 +0900

    practice react state

--- web/movie_app_2020/src/App.js
+++ web/movie_app_2020/src/App.js
@@ -1,22 +1,31 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({ favorite }) {
-  return <h3> I love {favorite} </h3>;
-}
+class App extends React.Component{
+  state = {
+    count : 0
+  };
 
-const foodILike = [{}]
+  add = () => {
+    this.setState(current =>({ count : current.count + 1}));
+  };
+  minus = () => {
+    this.setState(current =>({ count : current.count - 1}));
+  };
+  clear = () => {
+    this.setState(current =>({ count : current.count = 0}));
+  };
 
-function renderFood(dish) {
-  return <Food name={dish.name} picture={dish.image} />
-}
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(renderFood)}      
+  render() {
+    return (<div> 
+      <h1> The number is : {this.state.count} </h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>minus</button>
+      <button onClick={this.clear}>clear</button>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
