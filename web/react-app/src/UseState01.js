a432bc5e5cf2056d8eda6b885631c06f4659d72d
kimjinhyuk <kimjinhyuk2@gmail.com>
2020-10-28 09:02:33 +0900

    practice react hooks

--- web/react-app/src/UseState01.js
+++ web/react-app/src/UseState01.js
@@ -1,13 +1,15 @@
-const App = () => {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
-  return (
-    <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>Increment</button>
-      <button onClick={decrementItem}>Decrement</button>
-    </div>
-  );
-};
+// import React, { useState } from 'react';
+
+// const App = () => {
+//   const [item, setItem] = useState(1);
+//   const incrementItem = () => setItem(item + 1);
+//   const decrementItem = () => setItem(item - 1);
+//   return (
+//     <div className="App">
+//       <h1>Hello {item}</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//       <button onClick={incrementItem}>Increment</button>
+//       <button onClick={decrementItem}>Decrement</button>
+//     </div>
+//   );
+// };  
