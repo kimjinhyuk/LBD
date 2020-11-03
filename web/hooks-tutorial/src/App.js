49eac1f63c70d1ed647b9979c70c7a5dde27899f
kimjinhyuk <kimjinhyuk2@gmail.com>
2020-11-04 04:12:35 +0900

    practice React Hooks

--- web/hooks-tutorial/src/App.js
+++ web/hooks-tutorial/src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './Counter';
+import Info from './info';
+import ContextSample from './ContextSample';
+import Average from './Average';
+import UsePromiseSample from './UsePromiseSample';
+
+// const App = () => {
+
+//   const [visible, setVisible] = useState(false);
+
+//   return (
+//     <div>
+//       <button 
+//         onClick={() => {
+//           setVisible(!visible);
+//         }}>
+//           {visible ? '숨기기' : '보이기'}
+//       </button>
+//       <hr />
+//       {visible && <Info />}
+//     </div>
+//   );
+// };
 
 const App = () => {
-  return <Counter />;
+  return <UsePromiseSample />;
 };
 
 export default App;
---
+++ web/hooks-tutorial/src/info.js
@@ -0,0 +1,59 @@
+// import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value
+  };
+}
+
+
+const Info = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    name:'',
+    nickname:''
+  });
+  const {name, nickname} = state;
+  const onChange = e => {
+    dispatch(e.target);
+  };
+
+  // const [name, setName] = useState('');
+  // const [nickname, setNickname] = useState('');
+
+  // useEffect(() => {
+  //   console.log('effect');
+  //   console.log(name);
+  //   return () => {
+  //     console.log('cleanup');
+  //     console.log(name)
+  //   }
+  // });
+
+  // const onChangeName = e => {
+  //   setName(e.target.value);
+  // };
+  // const onChangeNickname = e => {
+  //   setNickname(e.target.value);
+  // };
+
+  return (
+    <div>
+      <div>
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
+      </div>
+      <div>
+        <div>
+          <b>이름 : </b> {name}
+        </div>
+        <div>
+          <b>닉네임 : </b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
