eb883dda6a6e89d5da764c93e3bc5b801ba6c9ad
kimjinhyuk <kimjinhyuk2@gmail.com>
2021-05-16 01:45:35 +0900

    add git tutorial 4,5

---
+++ web/tutorial-part-four/gatsby-config.js
@@ -0,0 +1,27 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+ module.exports = {
+  siteMetadata: {
+    title: `Pandas eating a lot`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
